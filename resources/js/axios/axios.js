833bc0246f4f9b89dd1aa88fe25c3c383552dbff
hiddendreamz <hiddendreamz7@gmail.com>
2020-04-05 22:14:54 +0545

    circle ci config file

--- resources/js/axios/axios.js
+++ resources/js/axios/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: 'https://aqueous-tor-49981.herokuapp.com/',
+    baseURL: 'https://aqueous-tor-49981.herokuapp.com/api/',
     timeout: 1000,
 });
 
