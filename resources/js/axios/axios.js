864a0b63f2c4bbd12002c5360207b5d67504de2a
hiddendreamz <hiddendreamz7@gmail.com>
2020-04-05 22:27:04 +0545

    fix

--- resources/js/axios/axios.js
+++ resources/js/axios/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: 'https://aqueous-tor-49981.herokuapp.com/api/',
+    baseURL: 'http://127.0.0.1:8000/api/',
     timeout: 1000,
 });
 
