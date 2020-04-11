d7739d78982756d6a98d2b993859c8a8bc59ba4c
hiddendreamz <hiddendreamz7@gmail.com>
2020-04-11 20:53:28 +0545

    api link changed

--- resources/js/axios/axios.js
+++ resources/js/axios/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: 'https://aqueous-tor-49981.herokuapp.com/api/',
+    baseURL: 'https://aafnobachat.herokuapp.com/api/',
 });
 
 export default instance
